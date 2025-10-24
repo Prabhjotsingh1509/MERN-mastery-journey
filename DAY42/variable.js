7f0923fe7fb240910e25e53a297de98a1e53cf17
Prabhjot Singh <amssre.16461@gmail.com>
2025-10-24 20:40:52 +0530

    datatypes and variables

---
+++ DAY42/variable.js
@@ -0,0 +1,27 @@
+/*var is not useful
+// var age=25;
+
+// if(true)
+// {
+    // console.log(age);
+// }
+
+// function solve ()
+// {
+    // var age =25;
+    // console.log(age);
+// }
+// 
+// solve();
+*/
+// let avoid to declare again and again same variable. 
+// reassignment is possible.
+{
+    let a = 10;
+    console.log(a);
+
+}
+// const cannot be declared 
+const b=25;
+console.log(b);
+// b=25; const value reassigment is not possible.
