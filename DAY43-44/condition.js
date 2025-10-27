1e6bab5f37ab60683b8e2509441b7bc83cf8a280
Prabhjot Singh <amssre.16461@gmail.com>
2025-10-27 23:56:05 +0530

    DAY43-44

---
+++ DAY43-44/condition.js
@@ -0,0 +1,24 @@
+let age = 53;
+if(age >18)
+{
+    console.log("can vote");
+}
+else{
+    console.log("Cannot vote");
+}
+let number =4;
+if(number == 1)
+{
+    console.log("A");
+}
+else if(number ==3)
+{
+    console.log("B");
+}
+else if(number==4)
+{
+    console.log("C");
+}
+else{
+    console.log("D");
+}
---
+++ DAY43-44/operators.js
@@ -0,0 +1,35 @@
+// Arithemetic Operators
+// binary
+let a=10;
+let b=5;
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a%b);
+console.log(a*b);
+console.log(a**b);
+// unary
+console.log(++b);
+console.log(a++);
+// Assignment Operators
+console.log(a+=10);
+console.log(a-=10);
+//comparision operators
+//loose operators
+console.log('5'==5);
+//strict operators
+console.log('5'===5);
+//Ternary operators
+let age=20;
+let status1=(age>18)? 'i can vote' : 'I cannot vote'
+console.log(status1);
+//logical operators
+let ans=(true && true && true)
+console.log(ans);
+let ans1=(false || false || true)
+console.log(ans1);
+let ans2=!(false);
+console.log(ans2);
+
+
+
